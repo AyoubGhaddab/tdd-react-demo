d61c13026fb292df823a7bfc66fab5d2b93838de
Ayoub <mohamedayoubghaddab@gmail.com>
2025-05-13 17:08:44 +0200

    setup pre commit lint

--- src/__tests__/Todo.test.tsx
+++ src/__tests__/Todo.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
-function Todo({ items }: { items: string[] }) {
+function todo({ items }: { items: string[] }) {
   return (
     <ul>
       {items.map(item => (
