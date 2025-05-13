4361537d905848e219e0f2d8a4c2aa85dc41736c
Ayoub <mohamedayoubghaddab@gmail.com>
2025-05-13 16:55:30 +0200

    setup eslint and antfu eslint in project

--- src/__tests__/Todo.test.tsx
+++ src/__tests__/Todo.test.tsx
@@ -1,30 +1,26 @@
-import {render,screen} from '@testing-library/react'
-import '@testing-library/jest-dom'
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 
 function Todo({ items }: { items: string[] }) {
   return (
     <ul>
-      {items.map((item, index) => (
-        <li key={index}>{item}</li>
+      {items.map(item => (
+        <li key={item}>{item}</li>
       ))}
     </ul>
-  )
+  );
 }
-
-describe('Buy some milk app', () => {
-    //1. render a item
-    //2. render multiple items
-    //3. add item to a list
-    //5. mark item as done
-    it('should render a todo item', () => {
-       // prepare or mock some data
-       // render the component
-       // check if the component is rendered
-       const todos = ['Buy some milk']
-       render(<Todo items={todos} />)
-       expect(screen.getByText('Buy some milk')).toBeInTheDocument()
-    }
-    )
-})
-
-
+describe("buy some milk app", () => {
+  // 1. render a item
+  // 2. render multiple items
+  // 3. add item to a list
+  // 5. mark item as done
+  it("should render a todo item", () => {
+    // prepare or mock some data
+    // render the component
+    // check if the component is rendered
+    const todos = ["Buy some milk"];
+    render(<Todo items={todos} />);
+    expect(screen.getByText("Buy some milk")).toBeInTheDocument();
+  });
+});
