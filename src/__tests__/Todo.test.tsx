878d6a88a0518c7d0c0b5db9da35229173185747
Ayoub <mohamedayoubghaddab@gmail.com>
2025-05-13 14:09:20 +0200

    ensure Todo component render a todo item

---
+++ src/__tests__/Todo.test.tsx
@@ -0,0 +1,30 @@
+import {render,screen} from '@testing-library/react'
+import '@testing-library/jest-dom'
+
+function Todo({ items }: { items: string[] }) {
+  return (
+    <ul>
+      {items.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+    </ul>
+  )
+}
+
+describe('Buy some milk app', () => {
+    //1. render a item
+    //2. render multiple items
+    //3. add item to a list
+    //5. mark item as done
+    it('should render a todo item', () => {
+       // prepare or mock some data
+       // render the component
+       // check if the component is rendered
+       const todos = ['Buy some milk']
+       render(<Todo items={todos} />)
+       expect(screen.getByText('Buy some milk')).toBeInTheDocument()
+    }
+    )
+})
+
+
