871c09609171752fb3c0cda22c7371d5a6a04e19
Ayoub <mohamedayoubghaddab@gmail.com>
2025-05-14 17:58:00 +0200

    decompose todo component to smaller SRP components

--- src/components/todo.tsx
+++ src/components/todo.tsx
@@ -1,33 +1,17 @@
 import { useState } from "react";
-import { v4 as uuid } from "uuid";
 
-type TodoItem = {
-  id: string;
-  content: string;
-  done?: boolean;
-};
+import type { TodoItem } from "../types/todo";
+
+import TodoInput from "./todo-input";
 
 export function Todo({ items }: { items: TodoItem[] }) {
-  const [todo, setTodo] = useState<string>("");
   const [todos, setTodos] = useState<TodoItem[]>(items);
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTodo(e.target.value);
-  };
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter") {
-      const id = uuid();
-      const newTodo: TodoItem = {
-        id,
-        content: todo,
-      };
-      setTodos([...todos, newTodo]);
-      setTodo("");
-    }
+  const onItemAdded = (item: TodoItem) => {
+    setTodos(prev => [...prev, item]);
   };
   return (
     <div>
-      <input data-testid="input" type="text" onChange={handleChange} onKeyDown={handleKeyDown} />
+      <TodoInput onItemAdded={onItemAdded} />
       <ul>
         {todos.map(item => (
           <li key={item.id}>{item.content}</li>
