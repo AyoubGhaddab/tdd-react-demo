b3a2c4867bfa317a4bb582ceef9f1afed8e0e51d
Ayoub <mohamedayoubghaddab@gmail.com>
2025-05-28 16:15:30 +0200

    remove comment

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 
 import "./index.css";
 import App from "./app.tsx";
-
+// hello world
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
